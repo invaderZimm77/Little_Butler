416ee074ba9760180ef59d71f210964f5fabd754
Terra <bclaycamp@gmail.com>
2021-12-13 13:31:53 -0500

    assocations set up

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ApplicationRecord
   belongs_to :employee
+  has_many :notes
 end
--- db/migrate/20211213160121_client_note.rb
+++
@@ -1,5 +0,0 @@
-class ClientNote < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :client, :note
-  end
-end
