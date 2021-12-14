429f4c9928ef5683c5596871c5413794bb0a4b61
Terra <bclaycamp@gmail.com>
2021-12-14 10:55:38 -0500

    settingup react

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(props) {
+	
+
+	return (
+		<>
+			
+		</>
+	)
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-# Employee.destroy_all
-# Client.destroy_all
+Client.destroy_all
+Employee.destroy_all
+
 
 @terra = Employee.create!(
   email: '00001TBC@mail.com',
@@ -21,6 +22,14 @@
 
 @bman = Client.create!(
 	name: 'Bruce Wayne',
+    address:'12345 Place Way, Statesville, DC 67890',
+    phone:'301-555-3456',
+    img_url:'https://www.lego.com/cdn/cs/catalog/assets/blt28bc9df041194ca5/1/70909_1to1_brucewayne_360_480.png',
+    employee_id:'00001TBC'
+)
+
+@brdman = Client.create!(
+	name: 'Harvey Birdman',
     address:'12345 Place Way, Statesville, DC 67890',
     phone:'301-555-3456',
     img_url:'https://www.lego.com/cdn/cs/catalog/assets/blt28bc9df041194ca5/1/70909_1to1_brucewayne_360_480.png',
