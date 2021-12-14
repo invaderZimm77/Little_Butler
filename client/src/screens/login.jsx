bc418239443a3ac168ef6490bdf5649cda20122c
Terra <bclaycamp@gmail.com>
2021-12-14 14:45:09 -0500

    save

--- client/src/screens/login.jsx
+++ client/src/screens/login.jsx
@@ -1,9 +1,38 @@
 import {useState} from 'react'
 
 export default function login() {
+	const [formData, setFormData] = useState({
+		employee_id: '',
+		password: ''
+	})
+	const {employee_id, password} = formData;
+
+	const handleChange = (e) => {
+
+		const {employee_id, value} = e.target;
+		setFormData(prevState+> ({
+			...prevState,
+			[employee_id]: value
+		}))
+	}
 	return (
-		<div>
-			
-		</div>
+		<form>
+			<label>Employee ID: 
+				<input 
+				type='text' 
+				name='employee_id' 
+				value={employee_id} 
+				onChange={handleChange}/>
+			</label>
+			<label>Password: 
+				<input 
+				type='password' 
+				name='password' 
+				value={password} 
+				onChange={handleChange}/>
+			</label>
+			<button>Submit</button>
+		</form>
+
 	)
 }
